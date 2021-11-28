88c1184f524b049de636e8c9f83ae1cfe60d6869
leafar396 <raphi.wirth@gmail.com>
2021-11-28 13:25:34 +0100

    created cart-item component and connected with reducer

--- src/components/cart-dropdown.jsx
+++ src/components/cart-dropdown.jsx
@@ -1,11 +1,13 @@
 import { connect } from 'react-redux';
 import { ReactComponent as ShoppingIcon } from '../assets/shopping-bag.svg'
 import Button from "../components/button";
+import CartItem from './cart-item';
 
-function CartDropdown({ currentUser }) {
+function CartDropdown({ cartItems }) {
   return (
     <div className="absolute w-60 h-80 flex flex-col p-5 border border-black bg-white top-20 right-0 z-10">
         <div className="h-60 flex flex-col overflow-y-scroll">
+          {cartItems.map(item => <CartItem key={item.id} item={item} />)}
         </div>
         <Button type="button" value="checkout"/>
     </div>
@@ -13,7 +15,7 @@ function CartDropdown({ currentUser }) {
 }
 
 const mapStateToProps = state => ({
-  currentUser: state.user.currentUser
+  cartItems: state.cart.cartItems
 });
 
 export default connect(mapStateToProps)(CartDropdown);
---
+++ src/components/cart-item.jsx
@@ -0,0 +1,22 @@
+import { connect } from "react-redux";
+import { addItem } from "../redux/cart/cart.actions";
+import Button from "./button";
+
+function CartItem({ item }) {
+    return (
+        <div className='w-full flex h-20 mb-4'>
+            <img className="w-1/3" src={item.imageUrl} alt={item.name} />
+            <div className="w-8/12 flex flex-col items-start pt-3 pb-5">
+                <span>{item.name}</span>
+                <span>{item.quantity} x {item.price$}</span>
+            </div>
+            
+        </div>
+    );
+}
+
+const mapDispatchToProps = dispatch => ({
+    addItem: item => dispatch(addItem(item))
+})
+
+export default connect(null, mapDispatchToProps)(CartItem);
